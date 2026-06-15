e2805d566aa026a21fd10f9ce64157f390a51f33
CameronWhyte32 <218741981+CameronWhyte32@users.noreply.github.com>
2026-06-15 12:23:57 +0100

    Etch a sketch project
    Updated to generate new grid based on user input

--- etc.js
+++ etc.js
@@ -1,25 +1,45 @@
 
 const gridContainer = document.querySelector(".container");
 const box = document.createElement("div");
-const resetBtn = document.querySelector(".reset")
-function createGrid() {
+const resetBtn = document.querySelector(".reset");
+const input = document.getElementById("grid-Size");
+const submitBtn = document.getElementById("submit");
+const gridSize = 960;
 
-    for (let i = 0; i < 256; i++) {
+
+submitBtn.addEventListener("click", () => {
+    if (input.value > 100 || input.value < 1){
+        return alert("Please enter a value between 1 and 100!");
+    }else {
+        const size = Number(input.value);
+        createGrid(size);
+    }
+
+})
+
+
+function createGrid(size) {
+
+    gridContainer.innerHTML = "";
+    const total = size * size;
+    const cellSize = 960 / size;
+
+    for (let i = 0; i < total; i++) {
         const box = document.createElement("div");
         box.classList.add("box");
-
-        
-        gridContainer.addEventListener("mouseover",  (e) => {
-            if(e.target.classList.contains("box")) {
-                e.target.style.backgroundColor = "black";
-            }
-        })
-
-
-    gridContainer.appendChild(box);
+        box.style.width = `${cellSize}px`;
+        box.style.height = `${cellSize}px`;
+        gridContainer.appendChild(box);
     }
     
 }
+        
+gridContainer.addEventListener("mouseover", (e) => {
+    if (e.target.classList.contains("box")) {
+        e.target.classList.add("hoverOver") 
+    }
+})
+
 
 
 resetBtn.addEventListener("click", resetPage)
@@ -27,12 +47,13 @@ resetBtn.addEventListener("click", resetPage)
 function resetPage() {
     const boxes = document.querySelectorAll(".box");
     
+
     boxes.forEach(box => {
-        box.style.backgroundColor = "white";
-    })
+        box.classList.remove("hoverOver");
+    });
 }
 
 
 
 
-createGrid();
+createGrid(16);
